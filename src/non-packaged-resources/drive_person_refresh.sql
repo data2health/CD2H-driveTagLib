f595e66d9b37596b719a61554060e1ed8dc294ca
eichmann <david-eichmann@uiowa.edu>
2019-08-16 20:27:41 +0000

    Updating transformation views to match changes to intake form.

--- src/non-packaged-resources/drive_person_refresh.sql
+++ src/non-packaged-resources/drive_person_refresh.sql
@@ -11,31 +11,31 @@ from drive.person;
 
 create or replace view drive.role_staging as
     select 1 as project_id, person_id,role
-    from (select row__::int as person_id, email_address,phase_ii_projects__institutional_informatics__research_it__a as role from drive.person) as foo
+    from (select row__::int as person_id, email_address,phase_ii_project_interest__institutional_informatics__resear as role from drive.person) as foo
     where role is not null and role != '' and role != 'Not involved'
 union
     select 2 as project_id, person_id,role
-    from (select row__::int as person_id, email_address,phase_ii_projects__ctsa_data_sharing_governance_pathways_ as role from drive.person) as foo
+    from (select row__::int as person_id, email_address,phase_ii_project_interest__ctsa_data_sharing_governance_path as role from drive.person) as foo
     where role is not null and role != '' and role != 'Not involved'
 union
     select 3 as project_id, person_id,role
-    from (select row__::int as person_id, email_address,phase_ii_projects__architecting_attribution_  as role from drive.person) as foo
+    from (select row__::int as person_id, email_address,phase_ii_project_interest__architecting_attribution_  as role from drive.person) as foo
     where role is not null and role != '' and role != 'Not involved'
 union
     select 4 as project_id, person_id,role
-    from (select row__::int as person_id, email_address,phase_ii_projects__educational_resource_and_competency_harmo as role from drive.person) as foo
+    from (select row__::int as person_id, email_address,phase_ii_project_interest__educational_resource_and_competen as role from drive.person) as foo
     where role is not null and role != '' and role != 'Not involved'
 union
     select 5 as project_id, person_id,role
-    from (select row__::int as person_id, email_address,phase_ii_projects__health_open_terminology_fhir_server_ as role from drive.person) as foo
+    from (select row__::int as person_id, email_address,phase_ii_project_interest__health_open_terminology_fhir_serv as role from drive.person) as foo
     where role is not null and role != '' and role != 'Not involved'
 union
     select 6 as project_id, person_id,role
-    from (select row__::int as person_id, email_address,phase_ii_projects__personas_for_clinical_and_translational_s as role from drive.person) as foo
+    from (select row__::int as person_id, email_address,phase_ii_project_interest__personas_for_clinical_and_transla as role from drive.person) as foo
     where role is not null and role != '' and role != 'Not involved'
 union
     select 7 as project_id, person_id,role
-    from (select row__::int as person_id, email_address,phase_ii_projects__reusable_data_best_practice_portal_ as role from drive.person) as foo
+    from (select row__::int as person_id, email_address,phase_ii_project_interest__reusable_data_best_practice_porta as role from drive.person) as foo
     where role is not null and role != '' and role != 'Not involved'
 union
     select 8 as project_id, person_id,role
@@ -43,27 +43,27 @@ union
     where role is not null and role != '' and role != 'Not involved'
 union
     select 9 as project_id, person_id,role
-    from (select row__::int as person_id, email_address,phase_ii_projects__data_quality_methods_and_tools_to_support as role from drive.person) as foo
+    from (select row__::int as person_id, email_address,phase_ii_project_interest__data_quality_methods_and_tools_to as role from drive.person) as foo
     where role is not null and role != '' and role != 'Not involved'
 union
     select 10 as project_id, person_id,role
-    from (select row__::int as person_id, email_address,phase_ii_projects__data_harmonization_ as role from drive.person) as foo
+    from (select row__::int as person_id, email_address,phase_ii_project_interest__data_harmonization_ as role from drive.person) as foo
     where role is not null and role != '' and role != 'Not involved'
 union
     select 11 as project_id, person_id,role
-    from (select row__::int as person_id, email_address,phase_ii_projects__loinc2hpo_semantic_phenotyping_tool_ as role from drive.person) as foo
+    from (select row__::int as person_id, email_address,phase_ii_project_interest__loinc2hpo_semantic_phenotyping_to as role from drive.person) as foo
     where role is not null and role != '' and role != 'Not involved'
 union
     select 12 as project_id, person_id,role
-    from (select row__::int as person_id, email_address,phase_ii_projects__menrva__an_interdisciplinary_open_researc as role from drive.person) as foo
+    from (select row__::int as person_id, email_address,phase_ii_project_interest__invenio_rdm__an_interdisciplinary as role from drive.person) as foo
     where role is not null and role != '' and role != 'Not involved'
 union
     select 13 as project_id, person_id,role
-    from (select row__::int as person_id, email_address,phase_ii_projects__patient_mortality_prediction_dream_challe as role from drive.person) as foo
+    from (select row__::int as person_id, email_address,phase_ii_project_interest__patient_mortality_prediction_drea as role from drive.person) as foo
     where role is not null and role != '' and role != 'Not involved'
 union
     select 14 as project_id, person_id,role
-    from (select row__::int as person_id, email_address,phase_ii_projects__science_of_translational_science_research as role from drive.person) as foo
+    from (select row__::int as person_id, email_address,phase_ii_project_interest__science_of_translational_science_ as role from drive.person) as foo
     where role is not null and role != '' and role != 'Not involved'
 union
     select 15 as project_id, person_id,role
@@ -71,11 +71,11 @@ union
     where role is not null and role != '' and role != 'Not involved'
 union
     select 16 as project_id, person_id,role
-    from (select row__::int as person_id, email_address,phase_ii_projects__competitions_tool_for_ctsa_community_peer as role from drive.person) as foo
+    from (select row__::int as person_id, email_address,phase_ii_project_interest__competitions_tool_for_ctsa_commun as role from drive.person) as foo
     where role is not null and role != '' and role != 'Not involved'
 union
     select 17 as project_id, person_id,role
-    from (select row__::int as person_id, email_address,phase_ii_projects__open_source_clinical_enterprise_data_ware as role from drive.person) as foo
+    from (select row__::int as person_id, email_address,phase_ii_project_interest__open_source_clinical_enterprise_d as role from drive.person) as foo
     where role is not null and role != '' and role != 'Not involved'
 union
     select 18 as project_id, person_id,role

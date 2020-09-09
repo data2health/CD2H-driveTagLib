ed24cccfcb74572e168f6e9764c97bb763a91ec2
eichmann <david-eichmann@uiowa.edu>
2020-09-09 19:52:46 +0000

    Pretty much everything in place to push registration data to GSuite.

--- src/main/java/org/cd2h/drive/util/N3CRegPusher.java
+++ src/main/java/org/cd2h/drive/util/N3CRegPusher.java
@@ -30,31 +30,12 @@ public class N3CRegPusher extends Sheet {
 	List<List<Object>> values = new ArrayList<List<Object>>();
 	int rowCount = 0;
 	
-	PreparedStatement stmt = conn.prepareStatement("select registration.email,official_first_name,official_last_name,first_name,last_name,coalesce(ror_id,''),ror_name,'',orcid_id,gsuite_email,slack_id,github_id,twitter_id,expertise,therapeutic_area,assistant_email,enclave,workstreams,created,updated,official_full_name,official_institution,emailed from n3c_admin.registration left outer join n3c_admin.user_org_map on registration.email=user_org_map.email order by last_name,first_name");
+	PreparedStatement stmt = conn.prepareStatement("select * from n3c_admin.gsuite_view");
 	ResultSet rs = stmt.executeQuery();
 	while (rs.next()) {
 	    List<Object> resultRow = new ArrayList<Object>();
-	    resultRow.add(rs.getString(1));
-	    resultRow.add(rs.getString(2));
-	    resultRow.add(rs.getString(3));
-	    resultRow.add(rs.getString(4));
-	    resultRow.add(rs.getString(5));
-	    resultRow.add(rs.getString(6));
-	    resultRow.add(rs.getString(7));
-	    resultRow.add(rs.getString(8));
-	    resultRow.add(rs.getString(9));
-	    resultRow.add(rs.getString(10));
-	    resultRow.add(rs.getString(11));
-	    resultRow.add(rs.getString(12));
-	    resultRow.add(rs.getString(13));
-	    resultRow.add(rs.getString(14));
-	    resultRow.add(rs.getString(15));
-	    resultRow.add(rs.getString(16));
-	    resultRow.add(rs.getString(17));
-	    resultRow.add(rs.getString(18));
-	    resultRow.add(rs.getString(19));
-	    resultRow.add(rs.getString(20));
-	    resultRow.add(rs.getString(21));
+	    for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++)
+		resultRow.add(rs.getString(i));
 	    values.add(resultRow);
 	    rowCount++;
 	}
@@ -67,7 +48,7 @@ public class N3CRegPusher extends Sheet {
 	logger.info("skip count: " + prop_file.getProperty("sheets.skipcount"));
 	StringBuffer buffer = new StringBuffer(prop_file.getProperty("sheets.sheets")
 						+ "!A" + getStartRow(values)
-						+ ":AA" + getEndRow(values));
+						+ ":AC" + getEndRow(values));
 	logger.info("range: " + buffer.toString());
 	return buffer.toString();
     }

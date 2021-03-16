1335fe10cdc2518804f0dfa62dee13bfb0fb8f65
eichmann <david-eichmann@uiowa.edu>
2021-03-16 21:45:16 +0000

    Added support for pushing PASC submission to GSuite.

---
+++ src/main/java/org/cd2h/drive/util/PASCRegPusher.java
@@ -0,0 +1,69 @@
+package org.cd2h.drive.util;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.PropertyConfigurator;
+
+import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
+
+import edu.uiowa.extraction.PropertyLoader;
+
+public class PASCRegPusher extends Sheet {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException, GeneralSecurityException {
+	PropertyConfigurator.configure(args[0]);
+	prop_file = PropertyLoader.loadProperties("gsuite_pasc");
+	conn = getConnection();
+
+	List<List<Object>> values = buildValueList();
+	UpdateValuesResponse result = updateValues(prop_file.getProperty("sheets.spreadsheetId"), generateRange(values), "USER_ENTERED", values);
+	logger.info(result.getUpdatedCells() + " cells updated.");
+    }
+    
+    static List<List<Object>> buildValueList() throws SQLException {
+	List<List<Object>> values = new ArrayList<List<Object>>();
+	int rowCount = 0;
+	
+	PreparedStatement stmt = conn.prepareStatement("select * from pasc.submission");
+	ResultSet rs = stmt.executeQuery();
+	while (rs.next()) {
+	    List<Object> resultRow = new ArrayList<Object>();
+	    for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+		String value = rs.getString(i);
+		if (value == null)
+		    resultRow.add("");
+		else
+		    resultRow.add(value);
+	    }
+	    values.add(resultRow);
+	    rowCount++;
+	}
+	logger.info("returned: " + rowCount + "\tlist: " + values.size());
+	
+	return values;
+    }
+    
+    static String generateRange(List<List<Object>> values) {
+	logger.info("skip count: " + prop_file.getProperty("sheets.skipcount"));
+	StringBuffer buffer = new StringBuffer(prop_file.getProperty("sheets.sheets")
+						+ "!A" + getStartRow(values)
+						+ ":AF" + getEndRow(values));
+	logger.info("range: " + buffer.toString());
+	return buffer.toString();
+    }
+    
+    static int getStartRow(List<List<Object>> values) {
+	return Integer.parseInt(prop_file.getProperty("sheets.skipcount")) + 1;
+    }
+
+    static int getEndRow(List<List<Object>> values) {
+	return Integer.parseInt(prop_file.getProperty("sheets.skipcount")) + values.size();
+    }
+
+}
